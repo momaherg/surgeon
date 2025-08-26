0da464c6ceb867afa222f767c7aaf4e1ed2e7410
momaherg <momaherg@gmail.com>
2025-08-26 21:40:08 +0200

    ever later layers in the model

--- surgery_grid_search.py
+++ surgery_grid_search.py
@@ -669,7 +669,7 @@ def main():
     token_positions = list(range(0, 100, 3))  # Every 3rd token up to position 100
     
     # Hebbian parameters
-    layers_of_interest = [14, 15, 16]
+    layers_of_interest = [16, 17, 18]
     eta = 0.05
     mu = 1e-4
     window_size = 4  # Update 3 tokens at once
